15d1e2b7e3a69f5268f7dbbed08dc4c466de7599
Yazan Khatib <yazankhatib97@gmail.com>
2021-04-13 01:55:47 +0300

    fix: timezone for prayers difference

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,4 +1,4 @@
 export const SQLWhereClause = (columnName, offset, date) => {
-  const ans = `("${columnName}" ${offset[0]} interval '${offset[1]}${offset[2]}')::Date = '${date}'`;
+  const ans = `("${columnName}" ${offset[0]} interval '${offset[1]}${offset[2]} hours')::Date = '${date}'`;
   return ans;
 };
