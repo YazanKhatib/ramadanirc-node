2b41b82e724ba05d91ea7db53e77f25f5011193f
Zain Khatib <zainkhatib9@gmail.com>
2021-08-08 00:15:09 +0300

    fix: SQL command for PSQL 13.1

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,5 +1,5 @@
 export const SQLWhereClause = (columnName, offset, date) => {
-  // const ans = `("${columnName}" ${offset[0]} interval '${offset[1]}${offset[2]} hours')::Date = '${date}'`;
-  const ans = `("${columnName}")::Date = '${date}'`;
+  const ans = `("${columnName}" ${offset[0]} interval '${offset[1]}${offset[2]} hours')::Date = '${date}'`;
+  //const ans = `("${columnName}")::Date = '${date}'`;
   return ans;
 };
